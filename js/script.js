5a2b243c744d7f78023fc65331ac6bd93a3cb820
2xCO <corinna.kunst@icloud.com>
2025-10-14 22:59:56 +0200

    transform geht

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+// window.addEventListener('scroll', () => {
+//   const scroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+//   // Snap auf 0, 0.5 oder 1
+//   const snapped = scroll < 0.25 ? 0 : scroll < 0.75 ? 0.5 : 1;
+//   document.body.style.setProperty('--scroll', snapped);
+// }, false);
 window.addEventListener('scroll', () => {
-  const scroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-  // Snap auf 0, 0.5 oder 1
-  const snapped = scroll < 0.25 ? 0 : scroll < 0.75 ? 0.5 : 1;
-  document.body.style.setProperty('--scroll', snapped);
+  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
